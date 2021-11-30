b66080874ac657b1bbdd7a9181c89e5d0afcb585
khyperia <953151+khyperia@users.noreply.github.com>
2021-11-30 14:50:33 +0100

    update wgpu

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 pub async fn create_device() -> (wgpu::Device, wgpu::Queue) {
-    let instance = wgpu::Instance::new(wgpu::BackendBit::PRIMARY);
+    let instance = wgpu::Instance::new(wgpu::Backends::VULKAN);
     let adapter = instance.request_adapter(&Default::default()).await.unwrap();
-    adapter
-        .request_device(&Default::default(), None)
-        .await
-        .unwrap()
-}
-unsafe fn u8_as_u32_slice(p: &[u8]) -> &[u32] {
-    std::slice::from_raw_parts(p.as_ptr() as *const u32, p.len() / 4)
+    let dd = wgpu::DeviceDescriptor {
+        label: None,
+        features: wgpu::Features::SPIRV_SHADER_PASSTHROUGH,
+        limits: Default::default(),
+    };
+    adapter.request_device(&dd, None).await.unwrap()
 }
 fn main() {
     env_logger::init();
@@ -15,10 +14,11 @@ fn main() {
     for path in std::env::args().skip(1) {
         println!("{}", path);
         let spirv = std::fs::read(path).unwrap();
-        let _module = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
-            label: None,
-            source: wgpu::ShaderSource::SpirV(unsafe { u8_as_u32_slice(&spirv) }.into()),
-            flags: wgpu::ShaderFlags::VALIDATION,
-        });
+        let _module = unsafe {
+            device.create_shader_module_spirv(&wgpu::ShaderModuleDescriptorSpirV {
+                label: None,
+                source: wgpu::util::make_spirv_raw(&spirv),
+            })
+        };
     }
 }
